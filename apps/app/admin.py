c472a90c1be58067caed27538191815443185ea7
osw4l <ioswxd@gmail.com>
2019-09-12 17:58:42 -0500

    first migration ready

--- apps/app/admin.py
+++ apps/app/admin.py
@@ -3,6 +3,19 @@ from . import models
 # Register your models here.
 
 
-admin.site.site_header = 'Django osw4l boilerplate'
-admin.site.site_title = 'Django osw4l boilerplate'
-admin.site.index_title = 'Django osw4l boilerplate'
+admin.site.site_header = 'LiveSet Admin'
+admin.site.site_title = 'LiveSet Admin'
+admin.site.index_title = 'LiveSet Admin'
+
+COMMON_LIST_DISPLAY = ['id', 'name', 'order', 'file']
+
+
+@admin.register(models.Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = COMMON_LIST_DISPLAY
+
+
+@admin.register(models.Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = COMMON_LIST_DISPLAY
+
--- apps/app/models.py
+++ apps/app/models.py
@@ -5,3 +5,27 @@ from . import constants
 
 # Create your models here.
 
+
+class ResourceModel(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    order = models.PositiveIntegerField(default=0)
+    file = models.FileField()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Song(ResourceModel):
+    class Meta:
+        verbose_name = 'Song'
+        verbose_name_plural = 'Songs'
+
+
+class Video(ResourceModel):
+    class Meta:
+        verbose_name = 'Video'
+        verbose_name_plural = 'Videos'
+
